415ccf772236ef2fdf846e93ffd06a1a44b2025b
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2021-08-09 15:59:29 +0900

    Added morgan

--- src/server.js
+++ src/server.js
@@ -1,8 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+
+const home = (req, res) => {
+  return res.send("hello");
+};
+const login = (req, res) => {
+  return res.send("login");
+};
+
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () =>
   console.log(`Server Listening on port http://localhost:${PORT}`);
