1f2048a4ab98f519b1cd70c0c6d46e649ba9c69e
Jang Wook Pyun <42465137+pyun0825@users.noreply.github.com>
2021-08-27 22:06:42 +0900

    Fake DB & post controller added

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import { watch, getEdit, postEdit } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
